de8c0b50178dc557de77661d83fd36daa7c2b692
Moh.Alkasem <167920527+KasemTec@users.noreply.github.com>
2024-11-21 11:56:29 +0100

    Routes
    Define and configure Routes in app modules

--- 03-forntend/angular-ecommerce/src/app/app.module.ts
+++ 03-forntend/angular-ecommerce/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -6,12 +7,26 @@ import { ProductListComponent } from './component/product-list/product-list.comp
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
+
+
+// Define Router
+// When path matches, create new instance of the component
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent },
+  {path: 'category', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' }, // Genirc will match on anything that did't match above routes
+
+];
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    // Configure Routes
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
     //  HttpClientModule    no longer need in Angular 18, instead add provideHttpClient() in providers
